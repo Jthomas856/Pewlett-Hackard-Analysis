c0e63cf4fd51ac62c5cefb84c32b4efc5e64d518
Jordan Thomas <jkt856@gmail.com>
2021-10-14 19:57:34 -0400

    Adding schema and queries.

--- schema.sql
+++ schema.sql
@@ -37,11 +37,11 @@ CREATE TABLE salaries (
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR(40) NOT NULL,
+	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, from_date)
 );
 
 CREATE TABLE dept_emp (
@@ -54,4 +54,4 @@ CREATE TABLE dept_emp (
 		PRIMARY KEY (emp_no, dept_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM titles;
