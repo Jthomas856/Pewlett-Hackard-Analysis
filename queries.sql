2968bf1adbf6d3dff6239a409e8c9b5c29527ab0
Jordan Thomas <jkt856@gmail.com>
2021-10-23 16:00:04 -0400

    Adding deliverable 1 data.

--- queries.sql
+++ queries.sql
@@ -149,3 +149,28 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
+
+-- Skill Drill, list of employees in Sales and Sales and Development
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no,
+	dpt.dept_name
+FROM retirement_info AS ri
+	Inner Join dept_emp AS de
+		ON (ri.emp_no = de.emp_no)
+	INNER JOIN departments AS dpt
+		ON (de.dept_no = dpt.dept_no)
+WHERE dept_name = 'Sales';
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.dept_no,
+	dpt.dept_name
+FROM retirement_info AS ri
+	INNER JOIN dept_emp AS de
+		ON (ri.emp_no = de.emp_no)
+	INNER JOIN departments AS dpt
+		ON (de.dept_no = dpt.dept_no)
+WHERE dept_name IN ('Sales', 'Development');
